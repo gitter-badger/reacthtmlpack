aba9461553cca5e2e1f95fa7f3ebd5a4064075a8
Tom Chen <developer@tomchentw.com>
2015-08-24 15:42:03 +0800

    refactor(bin): format description and outputHelp when no command

--- bin/index.js
+++ bin/index.js
@@ -8,16 +8,31 @@ program.version(require("../package.json").version);
 
 program
   .command("buildToDir <destDir> <srcPatterns...>")
-  .description("build all files that matches srcPatterns and writes the output htmls into destDir. The relative of these files to destDir is determined by the relative path of the source to srcPatterns")
+  .description("\n\t\
+build all files that matches srcPatterns and writes the generated htmls into destDir.\
+The relative of these files to destDir is determined by\
+the relative path of the source to srcPatterns\n"
+  )
   .action(cli.buildToDir);
 
 program
   .command("watchAndBuildToDir <destDir> <srcPatterns...>")
-  .description("Watch and build all files that matches srcPatterns and writes the output htmls into destDir. The relative of these files to destDir is determined by the relative path of the source to srcPatterns")
+  .description("\n\t\
+Watch and build all files that matches srcPatterns\
+and writes the output htmls into destDir. The relative of these files to destDir\
+is determined by the relative path of the source to srcPatterns\n"
+  )
   .action(cli.watchAndBuildToDir);
 
 program.parse(process.argv);
 
-if (!process.argv.slice(2).length) {
+var emptyArgs = !program.args.length;
+var notWhitelistCommand = program.commands.some(function (command) {
+  return command.name === program.args[0];
+});
+
+var shouldOutputHelp = [emptyArgs, notWhitelistCommand].some(function (it) { return it; });
+
+if (shouldOutputHelp) {
   program.outputHelp();
 }
