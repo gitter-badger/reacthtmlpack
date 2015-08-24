858cb5c21cd09a6be4e4eea7c880ee21304bcf1f
Tom Chen <developer@tomchentw.com>
2015-08-24 11:51:23 +0800

    refactor(cli): extract to functions

--- src/cli.js
+++ src/cli.js
@@ -32,57 +32,16 @@ import {
   chunkList$ToStaticMarkup$,
 } from "./core";
 
-
 const writeFile = Observable.fromNodeCallback(nodeWriteFile);
 
-export function replaceWithHtmlExt (filepath) {
-  const dirpath = extractDirname(filepath);
-
-  let basename = extractBasename(filepath);
-
-  while (true) {
-    const ext = extractExtname(basename);
-    if (ext) {
-      basename = extractBasename(basename, ext);
-    } else {
-      return resolvePath(dirpath, `${ basename }.html`);
-    }
-  }
-}
-
-export function buildToDir (destDir, srcPatterns) {
-  const matchesFilepath$ = Observable.fromArray(srcPatterns)
-    .selectMany(srcPattern => {
-      const globber = new Glob(srcPattern);
-      const base = glob2base(globber);
-
-      return Rx.Observable.create(function (observer) {
-        globber.once("end", matches => {
-          observer.onNext({
-            base,
-            matches,
-          });
-          observer.onCompleted();
-        });
-      });
-    })
-    .reduce((acc, {base, matches}) => {
-      acc.matches.push(...matches);
-      matches.forEach(match => {
-
-        const filepath = replaceWithHtmlExt(match);
-        acc.relativePathByMatch[match] = relativePathOf(base, filepath);
-      });
-
-      return acc;
-    }, {matches: [], relativePathByMatch: {}})
-    .first();
+/**
+ * @public
+ */
+export function buildToDir (destDir, srcPatternList) {
+  const matchesFilepath$ = getMatchesFilepath$(srcPatternList);
 
   return Observable.of(matchesFilepath$)
-    .map(matchesFilepath$ => 
-      matchesFilepath$
-        .selectMany(({matches}) => Observable.fromArray(matches))
-    )
+    .map(matchesFilepath$ToFilepath$)
     .map(filepath$ToBabelResult$)
     .map(babelResult$ToReactElement$)
     .map(reactElement$ToChunkList$)
@@ -113,4 +72,76 @@ export function buildToDir (destDir, srcPatterns) {
         () => { console.log("done!"); }
       );
     });
+}
+
+/**
+ * @private
+ */
+export function getMatchesFilepath$ (srcPatternList) {
+  return Observable.fromArray(srcPatternList)
+    .selectMany(srcPatternToMatchResult)
+    .reduce(matchResultToMatchesFilepathReducer, {matches: [], relativePathByMatch: {}})
+    .first();
+}
+
+/**
+ * @private
+ */
+export function srcPatternToMatchResult (srcPattern) {
+  const globber = new Glob(srcPattern);
+  const base = glob2base(globber);
+
+  return Rx.Observable.create(function (observer) {
+    function callback (matches) {
+      observer.onNext({
+        base,
+        matches,
+      });
+      observer.onCompleted();
+    };
+
+    globber.once("end", callback);
+
+    return globber.removeListener.bind(globber, "end", callback);
+  });
+}
+
+/**
+ * @private
+ */
+export function replaceWithHtmlExt (filepath) {
+  const dirpath = extractDirname(filepath);
+
+  let basename = extractBasename(filepath);
+
+  while (true) {
+    const ext = extractExtname(basename);
+    if (ext) {
+      basename = extractBasename(basename, ext);
+    } else {
+      return resolvePath(dirpath, `${ basename }.html`);
+    }
+  }
+}
+
+/**
+ * @private
+ */
+export function matchResultToMatchesFilepathReducer (acc, {base, matches}) {
+  acc.matches.push(...matches);
+  matches.forEach(match => {
+
+    const filepath = replaceWithHtmlExt(match);
+    acc.relativePathByMatch[match] = relativePathOf(base, filepath);
+  });
+
+  return acc;
+}
+
+/**
+ * @private
+ */
+export function matchesFilepath$ToFilepath$ (matchesFilepath$) {
+  return matchesFilepath$
+    .selectMany(({matches}) => Observable.fromArray(matches))
 }
