7fda99c073a0f974737083434e3a0c00f556b3cf
Tom Chen <developer@tomchentw.com>
2015-08-26 16:34:17 +0800

    docs(SimpleAssets): add build script

--- examples/SimpleAssets/SimpleAssets.webpackConfig.js
+++ examples/SimpleAssets/SimpleAssets.webpackConfig.js
@@ -4,6 +4,18 @@ var Path = require("path");
 var webpack = require("webpack");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
+var PRODUCTION_PLUGINS;
+
+if ("production" === process.env.NODE_ENV) {
+  PRODUCTION_PLUGINS = [
+    // Safe effect as webpack -p
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+  ];
+} else {
+  PRODUCTION_PLUGINS = [];
+}
+
 module.exports = {
   context: __dirname,
   output: {
@@ -27,6 +39,6 @@ module.exports = {
   },
   plugins: [
     new webpack.EnvironmentPlugin("NODE_ENV"),
-    new ExtractTextPlugin("[name].css", {disable: false}),
-  ],
+    new ExtractTextPlugin("[name]-[chunkhash].css", {disable: false}),
+  ].concat(PRODUCTION_PLUGINS),
 };
